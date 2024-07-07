7939c91387caeeee51c94a9b53dda76804c75e23
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-07-07 20:59:46 +0530

    occurence

---
+++ Myproject/src/arrayprograms/OccurenceOfElements.java
@@ -0,0 +1,34 @@
+package arrayprograms;
+
+import java.util.Scanner;
+
+public class OccurenceOfElements {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		  int n=scan.nextInt();
+		    int a[]=new int[n];
+		    for(int i=0;i<a.length;i++)
+		    {
+		    	a[i]=scan.nextInt();
+		    }
+		    
+		    int count=1;
+		    for(int i=0;i<a.length-1;i++)
+		    {
+		    	if(a[i]==a[i+1])
+		    	{
+		    		count++;
+		    	}
+		    	else
+		    	{
+		    		System.out.println(a[i]+"-"+count);
+		    		count=1;
+		    	}
+		    }
+		    System.out.println(a[a.length-1]+"-"+count);
+
+	}
+
+}
