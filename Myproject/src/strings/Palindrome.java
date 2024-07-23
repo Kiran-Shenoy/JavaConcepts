7fbbf420f77c873ed7cdbb5aa0a83191319a3634
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-07-23 21:35:28 +0530

    palin

--- Myproject/src/strings/Palindrome.java
+++ Myproject/src/strings/Palindrome.java
@@ -9,8 +9,42 @@ public class Palindrome {
 
 
 		String s=scan.next();
+//
+//		String res=reverse(s);
+//		if(s.equals(res))
+//		{
+//			System.out.println("Palindrome");
+//		}
+//		else
+//		{
+//			System.out.println("Not Palindrome");
+//		}
+//	}
+//
+//	public static String reverse(String s)
+//	{
+//		int i=0,j=s.length()-1;
+//		while(i<j)
+//		{
+//			if(s.charAt(i)==s.charAt(j))
+//			{
+//				i++;
+//				j--;
+//
+//			}
+//			else
+//			{
+//				return "Not Palindrome";
+//			}
+//		}
+//		return "Palindrome";
 
-		String res=reverse(s);
+		
+		String res="";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			res=res+s.charAt(i);
+		}
 		if(s.equals(res))
 		{
 			System.out.println("Palindrome");
@@ -19,26 +53,6 @@ public class Palindrome {
 		{
 			System.out.println("Not Palindrome");
 		}
-	}
-
-	public static String reverse(String s)
-	{
-		int i=0,j=s.length()-1;
-		while(i<j)
-		{
-			if(s.charAt(i)==s.charAt(j))
-			{
-				i++;
-				j--;
-
-			}
-			else
-			{
-				return "Not Palindrome";
-			}
-		}
-		return "Palindrome";
-
 
 	}
 
