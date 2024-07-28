5c3d1557172106a4dd85f0e2e5e1c8d2c4b90606
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-07-28 22:01:01 +0530

    lastDigit

---
+++ Myproject/src/arrayprograms/Sorting.java
@@ -0,0 +1,15 @@
+package arrayprograms;
+
+import java.util.Scanner;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		
+		int last=n%10;
+		System.out.println(last);
+	}
+
+}
