bedce74f3d9184ab44e19d1a8b6050fd028d4fe8
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-08-05 21:22:49 +0530

    multiples

---
+++ Myproject/src/abstractClass/Multiples.java
@@ -0,0 +1,23 @@
+package abstractClass;
+
+import java.util.Scanner;
+
+public class Multiples {
+
+	public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+		
+		int n=scan.nextInt();
+		int x=scan.nextInt();
+		
+		for(int i=1;i<=x;i++)
+		{
+			if(i%n==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
