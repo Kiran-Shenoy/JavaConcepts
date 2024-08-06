0ea8abcf0122dae3b64f11897338b1f11910fa35
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-08-06 21:49:53 +0530

    multiple

--- Myproject/src/abstractClass/Multiples.java
+++ Myproject/src/abstractClass/Multiples.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Multiples {
 
 	public static void main(String[] args) {
-Scanner scan=new Scanner(System.in);
+    Scanner scan=new Scanner(System.in);
 		
 		int n=scan.nextInt();
 		int x=scan.nextInt();
