dca7af1cf73d2e22ff9502da8a4b23b19e5a921f
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-09-12 20:53:16 +0530

    remainder

---
+++ Myproject/src/abstractClass/Modulus.java
@@ -0,0 +1,20 @@
+package abstractClass;
+
+import java.util.Scanner;
+
+public class Modulus {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		int n=scan.nextInt();
+		int i=scan.nextInt();
+		
+		
+		int res = n%i;
+		
+		System.out.println(res);
+
+	}
+
+}
