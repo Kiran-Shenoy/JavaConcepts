24d42f174ab1e182af1971d483d3fb359f62989b
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-09-16 21:11:30 +0530

    oddArray

---
+++ Myproject/src/arrayprograms/OddArray.java
@@ -0,0 +1,32 @@
+package arrayprograms;
+
+import java.util.Scanner;
+
+public class OddArray {
+
+	public static void main(String[] args) {
+    Scanner scan=new Scanner(System.in);
+		
+		
+		int n=scan.nextInt();
+		
+		int a1[]=new int[n];
+		for(int i=0;i<a1.length;i++)
+		{
+			a1[i]=scan.nextInt();
+			
+		}
+		
+		
+		for(int i=0;i<a1.length;i++)
+		{
+			if(a1[i]%2!=0)
+			{
+				System.out.print(a1[i]+" ");
+			}
+		}
+
+
+	}
+
+}
