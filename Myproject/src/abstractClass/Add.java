5039f321327506a6effbde8adcdbd519f39d7698
Kiran Shenoy <kiranshenoy2001@gmail.com>
2024-10-16 20:47:00 +0530

    add

---
+++ Myproject/src/abstractClass/Add.java
@@ -0,0 +1,16 @@
+package abstractClass;
+
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		System.out.println(a+b);
+	}
+
+}
