23f7aa340b2ab85ed8020ac0e0ef3e233c4f078c
Kiran Shenoy <kiranshenoy2001@gmail.com>
2025-03-09 17:00:21 +0530

    int

---
+++ Myproject/src/methodOverload/Hi.java
@@ -0,0 +1,14 @@
+package methodOverload;
+
+import java.util.Scanner;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+int a = scanner.nextInt();
+System.out.println(a);
+	}
+
+}
