7cd5c3e6aaf0b785c68f226371646020d7dd26cc
Kiran Shenoy <kiranshenoy2001@gmail.com>
2025-12-02 21:54:02 +0530

    rotated array

---
+++ Myproject/src/arrayprograms/RotatedArray.java
@@ -0,0 +1,34 @@
+package arrayprograms;
+import java.util.Scanner;
+import java.util.Arrays;
+public class RotatedArray {
+
+	 public int findMin(int[] nums) {
+
+	        Arrays.sort(nums);
+
+	        for(int i=0;i<nums.length;i++)
+	        {
+	            return nums[i];
+	        }
+	        return -1;
+	        
+	    }
+	    public static void main(String[] args)
+	    {
+	        Scanner scan = new Scanner(System.in);
+	        RotatedArray s  = new RotatedArray();
+
+	        int n = scan.nextInt();
+
+	        int a[] = new int[n];
+	        for(int i=0;i<n;i++)
+	        {
+	            a[i] = scan.nextInt();
+	        }
+
+	        int r = s.findMin(a);
+
+	        System.out.print(r);
+	    }
+}
